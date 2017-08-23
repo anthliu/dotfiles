1bb91d564dec397f20bd11bd91afb0187595fbc7
Anthony Liu <liu916@gmail.com>
2017-08-23 12:42:56 -0400

    fix no song playing-error

--- .config/polybar/cmus.sh
+++ .config/polybar/cmus.sh
@@ -7,12 +7,17 @@ prepend_zero () {
 artist=$(echo -n $(cmus-remote -C status | grep artist -m 1 | rev | cut -c -40 | rev))
 song=$(echo -n $(cmus-remote -C status | grep title -m 1 | rev | cut -c -20 | rev))
 
-position=$(cmus-remote -C status | grep position | cut -c 10-)
-minutes1=$(prepend_zero $(($position / 60)))
-seconds1=$(prepend_zero $(($position % 60)))
+if [ -z "$artist" ]
+then
+    echo ""
+else
+    position=$(cmus-remote -C status | grep position | cut -c 10-)
+    minutes1=$(prepend_zero $(($position / 60)))
+    seconds1=$(prepend_zero $(($position % 60)))
 
-duration=$(cmus-remote -C status | grep duration | cut -c 10-)
-minutes2=$(prepend_zero $(($duration / 60)))
-seconds2=$(prepend_zero $(($duration % 60)))
+    duration=$(cmus-remote -C status | grep duration | cut -c 10-)
+    minutes2=$(prepend_zero $(($duration / 60)))
+    seconds2=$(prepend_zero $(($duration % 60)))
 
-echo -n "$artist  -  $song [$minutes1:$seconds1 / $minutes2:$seconds2]"
+    echo -n "$artist  -  $song [$minutes1:$seconds1 / $minutes2:$seconds2]"
+fi
